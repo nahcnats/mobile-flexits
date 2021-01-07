9eb9860e904d9b005c210e580b3103eff746ef65
Stanley Chan <nahcnats@gmail.com>
2021-01-07 13:11:12 +0800

    components/UI

---
+++ components/MapPreview.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const MapPreview = props => {
+  return (
+    <View>
+        <Text>MapPreview</Text>
+    </View>
+  );
+}
+export default MapPreview;
